b079e32bbe5ff7bc278c6d7c050eb45745709416
anthony <anthony_lin@genesis.com.tw>
2026-02-24 15:08:38 +0800

    adj

--- Link.bat
+++ Link.bat
@@ -1,3 +1,10 @@
-mklink /J "Q:\ZAC_Dev\Genesis_MVC\wwwroot" "p:\MyKata\MSW\src"
-rem mklink /D "Q:\ZAC_Dev\Genesis_MVC\wwwroot" "p:\MyKata\MSW\src"
+set tar_PJ=R:\MPI_Dev
+set src_path=p:\MyKata\MSW\src
+set tar_path=%tar_PJ%\Genesis_MVC\MSW
+set tar_js=%tar_PJ%\Genesis_MVC\mockServiceWorker.js
+
+mklink /J %tar_path%  %src_path%
+
+del %tar_js% \Q
+mklink %tar_js% p:\MyKata\MSW\mockServiceWorker.js 
 pause
